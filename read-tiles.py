86a37cac56cd0b72ddbbcedaa5231423cb339575
Draga Doncila Pop <ddon0001@student.monash.edu>
2020-03-25 10:22:00 +1100

    Added scale, naming images using suffix and scale

--- read-tiles.py
+++ read-tiles.py
@@ -23,6 +23,8 @@ IM_SUFFIXES = ['FRE_B11', 'FRE_B12', 'FRE_B2', 'FRE_B3', 'FRE_B4', 'FRE_B5', 'FR
 IM_SHAPES = [(5490, 5490), (5490, 5490), (10980, 10980), (10980, 10980), (10980, 10980), (5490, 5490), (5490, 5490), (5490, 5490), (10980, 10980), (5490, 5490), 
              (5490, 5490), (5490, 5490), (10980, 10980), (10980, 10980), (10980, 10980), (5490, 5490), (5490, 5490), (5490, 5490), (10980, 10980), (5490, 5490)]
 IM_DTYPE = np.dtype('int16')
+SCALES = [[20.0, 20.0], [20.0, 20.0], [10.0, 10.0], [10.0, 10.0], [10.0, 10.0], [20.0, 20.0], [20.0, 20.0], [20.0, 20.0], [10.0, 10.0], [20.0, 20.0], 
+          [20.0, 20.0], [20.0, 20.0], [10.0, 10.0], [10.0, 10.0], [10.0, 10.0], [20.0, 20.0], [20.0, 20.0], [20.0, 20.0], [10.0, 10.0], [20.0, 20.0]]
 
 # get all timestamps for each tile
 all_dirs = os.listdir(HOME_PATH)
@@ -40,7 +42,7 @@ i = len(timestamps)
 for timestamp, fn in timestamps:
     f_zip = zipfile.ZipFile(HOME_PATH + fn)
     
-    # open each tiff in this timestamp and append it to the grand array
+    # append each image to grand array
     for j, suffix in enumerate(IM_SUFFIXES):
         current_im = fn[:-4] + '/' + fn[:-4] + '_' + suffix + '.tif'
 
@@ -53,8 +55,9 @@ for timestamp, fn in timestamps:
     if i == 106:
         break;
 
+
 all_images = []
-# for each image of this resolution
+# stack all images
 for i, im_type in enumerate(grand_dask):
     all_images.append(
         da.stack(
@@ -64,11 +67,11 @@ for i, im_type in enumerate(grand_dask):
 
 with napari.gui_qt():
     start = time.time()
-    v = napari.view_image(all_images[0], name='Im 1', is_pyramid=False)
+    v = napari.view_image(all_images[0], name=IM_SUFFIXES[0] + '_' + str(SCALES[0]), is_pyramid=False, scale=SCALES[0])
     end_1 = time.time()
 
     for i, img in enumerate(all_images[1:]):
-        v.add_image(img, name="Im " + str(i+2), is_pyramid=False, visible=False)
+        v.add_image(img, name=IM_SUFFIXES[i+1] + '_' + str(SCALES[i+1]), is_pyramid=False, visible=False, scale=SCALES[i+1])
     end_2 = time.time()
 
     print("First image: ", end_1 - start)
